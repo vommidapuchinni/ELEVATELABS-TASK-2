6dc1719b6ddc9e00f3931847435742034520d46c
vommidapuchinni <161326469+vommidapuchinni@users.noreply.github.com>
2025-08-05 15:20:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 5000;
 const host = '0.0.0.0'; // <-- this is important
 
 const server = http.createServer((req, res) => {
-    res.end("Node.js App Deployed via Jenkins!");
+    res.end("ELEVATELABS TASK2!");
 });
 
 server.listen(port, host, () => {
