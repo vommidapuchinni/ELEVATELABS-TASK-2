2de51bcd0226b2e904237ad080f84f3b9b65cb0c
vommidapuchinni <161326469+vommidapuchinni@users.noreply.github.com>
2025-08-05 14:28:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+
 const http = require('http');
 const port = 6000;
+const host = '0.0.0.0'; // <-- this is important
+
 const server = http.createServer((req, res) => {
     res.end("Node.js App Deployed via Jenkins!");
 });
-server.listen(port, () => console.log(`Server running on port ${port}`));
 
+server.listen(port, host, () => {
+    console.log(`Server running at http://${host}:${port}`);
+});
